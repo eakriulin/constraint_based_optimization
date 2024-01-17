7b4a133c792b21bb49c926fe72175c4c653105aa
Eugene Kriulin <eakriulin@gmail.com>
2024-01-17 01:14:39 +0100

    Added independent mode

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     parser.add_argument('--height_age', action='store_true', help='If passed, the height_age nn will be trained and evaluated')
     parser.add_argument('--age_weight', action='store_true', help='If passed, the age_weight nn will be trained and evaluated')
     parser.add_argument('--height_weight', action='store_true', help='If passed, the height_weight nn will be trained and evaluated')
+    parser.add_argument('--independent', action='store_true', help='If passed, all three nns will be trained independently and evaluated')
     parser.add_argument('--constraint', action='store_true', help='If passed, all three nns will be trained in constraint mode and evaluated')
     args = parser.parse_args()
 
@@ -51,6 +52,22 @@ if __name__ == "__main__":
         print(f"height->weight nn: MSE = {nn_utils.eval(nn_height_weight, X_test_height, Y_test_weight)}")
 
         print("\nPrediction:")
+        print(f"height->weight nn: height {inputs_test_height[49:50][0][0]} cm -> weight {nn_utils.predict(nn_height_weight, X_test_height[49:50])[0][0]} kg")
+
+    if args.independent:
+        nn_utils.train(nn_height_age, X_train_height, Y_train_age, number_of_epochs=10000, learning_rate=0.001)
+        nn_utils.train(nn_age_weight, nn_utils.predict(nn_height_age, X_train_height), Y_train_weight, number_of_epochs=20000, learning_rate=0.005)
+        nn_utils.train(nn_height_weight, X_train_height, Y_train_weight, number_of_epochs=20000, learning_rate=0.005)
+        print("\nEvaluation:")
+        print(f"height->age->weight nn: MSE = {nn_utils.eval_chain(nn_height_age, nn_age_weight, X_test_height, Y_test_weight)}")
+        print(f"height->age nn: MSE = {nn_utils.eval(nn_height_age, X_test_height, Y_test_age)}")
+        print(f"age->weight nn: MSE = {nn_utils.eval(nn_age_weight, nn_utils.predict(nn_height_age, X_test_height), Y_test_weight)}")
+        print(f"height->weight nn: MSE = {nn_utils.eval(nn_height_weight, X_test_height, Y_test_weight)}")
+
+        print("\nPrediction:")
+        print(f"height->age->weight nn: height {inputs_test_height[49:50][0][0]} cm -> weight {nn_utils.predict_chain(nn_height_age, nn_age_weight, X_test_height[49:50])[0][0]} kg")
+        print(f"height->age nn: height {inputs_test_height[49:50][0][0]} cm -> age {nn_utils.predict(nn_height_age, X_test_height[49:50])[0][0]} years")
+        print(f"age->weight nn: age {inputs_test_age[49:50][0][0]} years -> weight {nn_utils.predict(nn_age_weight, nn_utils.predict(nn_height_age, X_test_height[49:50]))[0][0]} kg")
         print(f"height->weight nn: height {inputs_test_height[49:50][0][0]} cm -> weight {nn_utils.predict(nn_height_weight, X_test_height[49:50])[0][0]} kg")
 
     if args.constraint:
